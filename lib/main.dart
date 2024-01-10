9e2f8ffe4cb538bce446cbb339f235d4ac056eb5
Avadh K Sapariya <avadhksapariya.ak56du@gmail.com>
2024-01-10 23:43:27 +0530

    Navigation to details page, BottomNavigationBar added and some organizational changes in project.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:shoes_shop_app/home_page.dart';
+import 'package:shoes_shop_app/pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
