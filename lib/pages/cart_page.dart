9b4a611bb01ffdd42b6f3f86a2fcfc027e87d74d
Avadh K Sapariya <avadhksapariya.ak56du@gmail.com>
2024-01-11 21:21:11 +0530

    Cart page UI created.

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,13 +1,33 @@
 import 'package:flutter/material.dart';
 
+import '../dummy_data.dart';
+
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Cart Page'),
+      appBar: AppBar(
+        title: const Text('Cart'),
+      ),
+      body: ListView.builder(
+        itemCount: cart.length,
+        itemBuilder: (context, index) {
+          final cartItem = cart[index];
+          return ListTile(
+            leading: CircleAvatar(
+              backgroundImage: AssetImage(cartItem['imageUrl'].toString()),
+              radius: 30,
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.delete, color: Colors.red,),
+            ),
+            title: Text(cartItem['title'].toString(), style: Theme.of(context).textTheme.bodySmall,),
+            subtitle: Text('Size: ${cartItem['size']}'),
+          );
+        },
       ),
     );
   }
